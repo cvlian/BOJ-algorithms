598263514c23c6ee9aa1d63096112aa3a6ab276c
cvlian <cvlian09@gmail.com>
2021-03-29 16:12:34 +0900

    python:segTree:merge sort tree

--- python/segTree.py
+++ python/segTree.py
@@ -35,3 +35,33 @@ class segTree:
         while p > 1:
             p //= 2
             self.v[p] = self.op(self.v[2*p], self.v[2*p + 1])
+
+"""
+    Merge Sort Tree
+"""
+
+class mergeTree:
+
+    def __init__(self, n):
+        self.size = 2**(math.ceil(math.log(n, 2)) + 1)
+        self.v = [[] for _ in range(self.size)]
+
+    def add(self, p, v):
+        p += self.size//2 - 1
+        self.v[p].append(v)
+
+    def fill(self, p):
+        if p >= self.size//2:
+            return self.v[p]
+        
+        self.v[p] = sorted(self.fill(2*p)+self.fill(2*p+1))
+        return self.v[p]
+
+    def cal(self, p, L, R, nodeL, nodeR, k):
+        if R < nodeL or nodeR < L:
+            return 0
+        if L <= nodeL and nodeR <= R:
+            return self.v[p]
+
+        m = (nodeL + nodeR)//2
+        return self.cal(2*p, L, R, nodeL, m, k) + self.cal(2*p + 1, L, R, m + 1, nodeR, k)
